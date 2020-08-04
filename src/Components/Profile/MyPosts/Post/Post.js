043cd4c4a899e79d7108eab09e816bf8c2cfa91f
Ekaterina Simonova <simonovaekaterina227@mail.ru>
2020-08-04 04:49:28 +0300

    Added component dividing blocks to the "profile" Such as the:   my posts are responsible for the block of posts,   post - are responsible for the block of one post,   profile info - responsible for the main content on the page

---
+++ src/Components/Profile/MyPosts/Post/Post.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Profile.module.css';
+
+function Profile() {
+    return (
+        <div>
+          Hello!
+        </div>
+    );
+}
+
+export default Profile;
