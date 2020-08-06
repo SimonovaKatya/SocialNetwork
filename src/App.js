aa5a73f48f5bd81487798e0e832e06bbf0f4a85f
Ekaterina Simonova <simonovaekaterina227@mail.ru>
2020-08-06 04:40:27 +0300

    Added a preset for the navbar added a header added settings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.module.css';
 import Profile from "./Components/Profile/Profile";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div>
+        <Header/>
         <Profile/>
     </div>
   );
---
+++ src/Components/Settings/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Profile.module.css';
+
+function Settings() {
+    return (
+        <div>
+          Hello!
+        </div>
+    );
+}
+
+export default Settings;
