09533cfb59b9fc0db6eeb4399f970b4f4e79a7d2
Ekaterina Simonova <simonovaekaterina227@mail.ru>
2020-08-08 01:39:00 +0300

    Created all templates for basic components: Profile Messages Friends News Music Videos Games Settings Header Navbar

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import  classes from './Navbar.module.css';
+
+const Navbar = () => {
+    return <header className={classes.header}>
+        <img
+            src="https://blog.marketveep.com/hs-fs/hubfs/fanta%20logo.png?width=163&name=fanta%20logo.png"/>
+    </header>
+}
+
+export default Navbar;
