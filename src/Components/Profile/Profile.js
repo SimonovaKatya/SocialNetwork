137d29243fc82e168132ed50cde883b8ac9a3d26
Ekaterina Simonova <simonovaekaterina227@mail.ru>
2020-08-20 00:40:00 +0300

    Connected the profile info component to the main component

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Profile.module.css';
 import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 function Profile() {
     return (
         <div>
+            <ProfileInfo/>
             <MyPosts/>
         </div>
     );
