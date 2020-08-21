256a727f1ae8b703735c83e009f882445fba42f9
Ekaterina Simonova <simonovaekaterina227@mail.ru>
2020-08-21 15:02:44 +0300

    Divided the responsibility into two separate components dialog item and messages The principle of single responsibility is used. This is one of the principles of SOLID

---
+++ src/Components/Dialogs/DialogItem/DialogItem.js
@@ -0,0 +1,16 @@
+import classes from "../Dialogs.module.css";
+import {NavLink} from "react-router-dom";
+import React from "react";
+
+const DialogItem = (props) => {
+    let path = '/dialogs/' + props.id;
+
+    return (
+        <div className={classes.dialogs + '' + classes.active}>
+            <NavLink to={path}>  {props.name} </NavLink>
+
+        </div>
+    )
+}
+
+export default DialogItem;
--- src/Components/Dialogs/Dialogs.js
+++ src/Components/Dialogs/Dialogs.js
@@ -1,25 +1,9 @@
 import React from "react";
 import classes from './Dialogs.module.css';
-import {NavLink} from "react-router-dom";
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
-const DialogItem = (props) => {
-    let path = 'dialogs' + props.id;
 
-    return (
-        <div className={classes.dialogs + '' + classes.active}>
-            <NavLink to={path}>  {props.name} </NavLink>
-
-        </div>
-    )
-}
-
-const Message = (props) => {
-    return (
-        <div className={classes.message}>
-            {props.message}
-        </div>
-    )
-}
 
 const Dialogs = (props) => {
 
@@ -38,7 +22,7 @@ const Dialogs = (props) => {
         {id: 12, name: 'Valera'},
     ]
 
-    let dialogsElement = dialogsData.map( d => <DialogItem name={d.name} id={d.id}/>)
+    let dialogsElement = dialogsData.map(d => <DialogItem name={d.name} id={d.id}/>)
 
     let messagesData = [
         {id: 1, message: 'Hi'},
@@ -55,7 +39,7 @@ const Dialogs = (props) => {
         {id: 12, message: 'Yo'}
     ]
 
-    let messagesElement = messagesData.map( m => <Message message={m.message} id={m.id}/>)
+    let messagesElement = messagesData.map(m => <Message message={m.message} id={m.id}/>)
 
     return (
         <div className={classes.dialogs}>
