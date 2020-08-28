61b203cea9d33b2c23866eccecba349907d31100
Ekaterina Simonova <simonovaekaterina227@mail.ru>
2020-08-28 16:30:50 +0300

    Performed merging of two branches. Messages and Dev. She indicated that users can switch from messages to the profile and opened options for using other tools

--- src/Components/Dialogs/Dialogs.js
+++ src/Components/Dialogs/Dialogs.js
@@ -29,18 +29,30 @@ const Dialogs = (props) => {
         {id: 3, name: 'Sveta'},
         {id: 4, name: 'Sasha'},
         {id: 5, name: 'Victor'},
-        {id: 6, name: 'Valera'}
+        {id: 6, name: 'Valera'},
+        {id: 7, name: 'Vlad'},
+        {id: 8, name: 'Polina'},
+        {id: 9, name: 'Mark'},
+        {id: 10, name: 'Katy'},
+        {id: 11, name: 'Valera'},
+        {id: 12, name: 'Valera'},
     ]
 
     let dialogsElement = dialogsData.map( d => <DialogItem name={d.name} id={d.id}/>)
 
     let messagesData = [
         {id: 1, message: 'Hi'},
-        {id: 2, message: 'How your it-kamasutra?'},
+        {id: 2, message: 'How your?'},
         {id: 3, message: 'Yo'},
-        {id: 4, message: 'Yo'},
+        {id: 4, message: 'Whht'},
         {id: 5, message: 'Yo'},
-        {id: 6, message: 'Yo'}
+        {id: 6, message: 'Hello'},
+        {id: 7, message: 'Yo'},
+        {id: 8, message: 'Hi'},
+        {id: 9, message: 'Yo'},
+        {id: 10, message: 'Yo'},
+        {id: 11, message: 'Yo'},
+        {id: 12, message: 'Yo'}
     ]
 
     let messagesElement = messagesData.map( m => <Message message={m.message} id={m.id}/>)
