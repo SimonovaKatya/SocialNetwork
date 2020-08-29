b81f13470600ff0a0756940298ece0ddf30f47da
Ekaterina Simonova <simonovaekaterina227@mail.ru>
2020-08-29 04:10:32 +0300

    Finished the page layout with the display of the user's friends.  Added navbar.  Created 4 friends per page. Used flex boxes

--- src/Components/Friends/Friends.js
+++ src/Components/Friends/Friends.js
@@ -3,15 +3,38 @@ import classes from './Friends.module.css';
 import ProfileInfo from "../Profile/ProfileInfo/ProfileInfo";
 import MyPosts from "../Profile/MyPosts/MyPosts";
 
+
 const Friends = () => {
     return (
         <div className={classes.content}>
-            <div className={classes.friend}>
-ажавладлаэвлвжаэлжаэлэалэалпжэвалжэалпэва
-            </div>
-            <div>
+
+            <div className={classes.friends}>
+                <div className={classes.search}>
+                    <img src="https://blueprintinsurance.ca/wp-content/uploads/2015/06/Review-150x150.jpg"/>
+                </div>
+
+
+                <div className={classes.friend}>
+                    <img src="https://mobimg.b-cdn.net/pic/v2/gallery/preview/oshki_oty_otiki-zhivotnye-43134.jpg"/>
+                </div>
+
+                <div className={classes.friend}>
+                    <img src="https://mobimg.b-cdn.net/pic/v2/gallery/preview/oshki_oty_otiki-zhivotnye-43134.jpg"/>
+                </div>
+
+                <div className={classes.friend}>
+                    <img src="https://mobimg.b-cdn.net/pic/v2/gallery/preview/oshki_oty_otiki-zhivotnye-43134.jpg"/>
+                </div>
+
+
+                <div className={classes.friend}>
+                    <img src="https://mobimg.b-cdn.net/pic/v2/gallery/preview/oshki_oty_otiki-zhivotnye-43134.jpg"/>
+                </div>
+
 
             </div>
+
+
         </div>
     );
 }
