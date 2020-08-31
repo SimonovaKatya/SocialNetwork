fed0081425c307f4853e2da24ada6f936e337484
Ekaterina Simonova <simonovaekaterina227@mail.ru>
2020-08-31 13:41:05 +0300

    Fixed layout. Add name and text-messages

--- src/Components/Friends/Friends.js
+++ src/Components/Friends/Friends.js
@@ -11,11 +11,16 @@ const Friends = () => {
             <div className={classes.friends}>
                 <div className={classes.search}>
                     <img src="https://blueprintinsurance.ca/wp-content/uploads/2015/06/Review-150x150.jpg"/>
+                    <div style={{marginTop: 15, marginLeft: 3}}>...</div>
                 </div>
 
 
                 <div className={classes.friend}>
                     <img src="https://mobimg.b-cdn.net/pic/v2/gallery/preview/oshki_oty_otiki-zhivotnye-43134.jpg"/>
+                    <div className={classes.info}>
+                        <div className={classes.name}>fsdf</div>
+                        <div className={classes.textMessage}>fdsfds</div>
+                    </div>
                 </div>
 
                 <div className={classes.friend}>
