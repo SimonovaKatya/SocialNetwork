526dcd2a17056aeeea16d51872d78abb4a816daa
Ekaterina Simonova <simonovaekaterina227@mail.ru>
2020-09-02 02:38:26 +0300

    I started making up the page for displaying music.  Used css-grid.  I created two large blocks for displaying all music and a search bar

--- src/Components/Music/Music.js
+++ src/Components/Music/Music.js
@@ -3,8 +3,26 @@ import classes from './Music.module.css';
 
 const Music = (props) => {
     return (
-        <div>
-            Music
+        <div className={classes.contentWrapper}>
+
+            <div className={classes.example}>r</div>
+            <div className={classes.search}>
+            <div className={classes.searchLine}>
+                Search
+            </div>
+            </div>
+
+            <div className={classes.allMusic}>
+                <div className={classes.music}>Music1</div>
+                <div className={classes.music}>Music2</div>
+                <div className={classes.music}>Music3</div>
+                <div className={classes.music}>Music4</div>
+                <div className={classes.music}>Music5</div>
+                <div className={classes.music}>Music5</div>
+                <div className={classes.music}>Music5</div>
+                <div className={classes.music}>Music5</div>
+            </div>
+
         </div>
     )
 }
