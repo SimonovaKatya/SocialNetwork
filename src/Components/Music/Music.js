84d432ba8c356ff4899b55882fc059a87b836763
Ekaterina Simonova <simonovaekaterina227@mail.ru>
2020-09-02 04:13:59 +0300

    Created new styles.Added icons for switching music. Added an icon for launching music and searching in the search bar. Aligned text to layouts

--- src/Components/Music/Music.js
+++ src/Components/Music/Music.js
@@ -5,22 +5,56 @@ const Music = (props) => {
     return (
         <div className={classes.contentWrapper}>
 
-            <div className={classes.example}>r</div>
             <div className={classes.search}>
-            <div className={classes.searchLine}>
-                Search
-            </div>
+                <div className={classes.searchLine}>
+                    <img src="https://static.thenounproject.com/png/41373-200.png"/>
+                    <div className={classes.text}>
+                        Search...
+                    </div>
+                </div>
             </div>
 
             <div className={classes.allMusic}>
-                <div className={classes.music}>Music1</div>
-                <div className={classes.music}>Music2</div>
-                <div className={classes.music}>Music3</div>
-                <div className={classes.music}>Music4</div>
-                <div className={classes.music}>Music5</div>
-                <div className={classes.music}>Music5</div>
-                <div className={classes.music}>Music5</div>
-                <div className={classes.music}>Music5</div>
+                <div className={classes.music}>
+                    <img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/play-175-304383.png"/>
+                    <img src="https://static.thenounproject.com/png/972079-200.png"/>
+                    Lorem Ipsum - это текст-"рыба"
+                </div>
+                <div className={classes.music}>
+                    <img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/play-175-304383.png"/>
+                    <img src="https://static.thenounproject.com/png/972079-200.png"/>
+                    Lorem Ipsum - это текст-"рыба"
+                </div>
+                <div className={classes.music}>
+                    <img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/play-175-304383.png"/>
+                    <img src="https://static.thenounproject.com/png/972079-200.png"/>
+                    Lorem Ipsum - это текст-"рыба"
+                </div>
+                <div className={classes.music}>
+                    <img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/play-175-304383.png"/>
+                    <img src="https://static.thenounproject.com/png/972079-200.png"/>
+                    Lorem Ipsum - это текст-"рыба"
+                </div>
+                <div className={classes.music}>
+                    <img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/play-175-304383.png"/>
+                    <img src="https://static.thenounproject.com/png/972079-200.png"/>
+                    Lorem Ipsum - это текст-"рыба"
+                </div>
+                <div className={classes.music}>
+                    <img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/play-175-304383.png"/>
+                    <img src="https://static.thenounproject.com/png/972079-200.png"/>
+                    Lorem Ipsum - это текст-"рыба"
+                </div>
+                <div className={classes.music}>
+                    <img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/play-175-304383.png"/>
+                    <img src="https://static.thenounproject.com/png/972079-200.png"/>
+                    Lorem Ipsum - это текст-"рыба"
+                </div>
+                <div className={classes.music}>
+                    <img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/play-175-304383.png"/>
+                    <img src="https://static.thenounproject.com/png/972079-200.png"/>
+                    Lorem Ipsum - это текст-"рыба"
+                </div>
             </div>
 
         </div>
