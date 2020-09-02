d422a89393e01167ea6cab280f4e1640d3031afe
Ekaterina Simonova <simonovaekaterina227@mail.ru>
2020-09-02 06:36:23 +0300

    Created a block for basic user information. Added an icon to your avatar. In the lower block for displaying friends, I added icons for them. Attached links from Internet resources. In the future, I will replace it with personal ones

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -2,36 +2,56 @@ import React from 'react';
 import classes from './Profile.module.css';
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import Post from "./MyPosts/Post/Post";
+
+const Post = (props) => {
+    return (
+        <div className={classes.item}>
+            <img
+                src="https://cdn.nlotv.com/i/image_1140x561/uploads/nlo/news/5e15a12b4a94c_thumb_1920_79588.jpeg"/>
+            {props.message}
+            <div>
+                <span>like {props.like}</span>
+            </div>
+
+        </div>
+    )
+}
 
 
-    let postData = [
-        {id: 1, message: 'Hi', likesCount: 17},
-        {id: 2, message: 'Hi', likesCount: 17},
-        {id: 3, message: 'Hi', likesCount: 17},
-        {id: 4, message: 'Hi', likesCount: 17},
-        {id: 5, message: 'Hi', likesCount: 17},
-        {id: 6, message: 'Hi', likesCount: 20},
-        {id: 7, message: 'Hi', likesCount: 17},
-        {id: 8, message: 'Hi', likesCount: 17},
-        {id: 9, message: 'Hi', likesCount: 27},
-        {id: 10, message: 'Hi', likesCount: 17},
+let postData = [
+    {id: 1, message: 'Hi', likesCount: 17},
+    {id: 2, message: 'Hi', likesCount: 17},
+    {id: 3, message: 'Hi', likesCount: 17},
+    {id: 4, message: 'Hi', likesCount: 17},
+]
 
-    ]
-
-    let postElement = postData.map( p => <Post message={p.message} like={p.likesCount}/>)
+let postElement = postData.map(p => <Post message={p.message} like={p.likesCount}/>)
 
 function Profile() {
     return (
         <div className={classes.wrapperContent}>
-                <div className={classes.avatar}>
-                    <img
-                        src='https://resimdiyari.com/_data/i/upload/2012/10/14/20121014183238-453f2df8-me.jpg'/>
-                </div>
+            <div className={classes.avatar}>
+                <img src='https://i.pinimg.com/736x/fa/04/0d/fa040d4fb07882160031efd57461b911.jpg'/>
+                AVATAR
+            </div>
 
-                 <div className={classes.descriptionBlock}>
+            <div className={classes.descriptionBlock}>
                 ava + description
-                 </div>
+            </div>
+
+            <div className={classes.friendList}>
+                <div className={classes.textFriends}>FRIEND</div>
+                <hr/>
+                <img src="https://colores.org.es/imagenes_colores/amarillo-pastel.jpg"/>
+                <img src="https://colores.org.es/imagenes_colores/amarillo-pastel.jpg"/>
+                <img src="https://colores.org.es/imagenes_colores/amarillo-pastel.jpg"/>
+                <img src="https://colores.org.es/imagenes_colores/amarillo-pastel.jpg"/>
+                <img src="https://colores.org.es/imagenes_colores/amarillo-pastel.jpg"/>
+            </div>
+
+            <div className={classes.personalInformation}>
+                INFORMATION
+            </div>
 
             <div className={classes.postsBlock}>
                 <h3>My post</h3>
